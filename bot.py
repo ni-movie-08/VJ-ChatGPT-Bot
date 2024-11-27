86668ef4cc53341f231ca8ed5256fe979706225d
──「 ʟᴜᴄɪғᴇʀ 」── <nimovie08@gmail.com>
2024-11-27 19:10:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,13 @@ class Bot(Client):
         me = await self.get_me()
         self.username = '@' + me.username
             
-        print('Bot Started Powered By @VJ_Botz')
+        print('Bot Started Powered By : 
+███╗░░██╗██╗
+████╗░██║██║
+██╔██╗██║██║
+██║╚████║██║
+██║░╚███║██║
+╚═╝░░╚══╝╚═╝')
 
 
     async def stop(self, *args):
